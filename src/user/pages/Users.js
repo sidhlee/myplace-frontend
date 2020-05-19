2b999037821e6083e7a893d181babbb4b35c9ac9
Hayoun Lee <sidhlee@gmail.com>
2020-05-19 15:34:57 +0900

    39. Adding a UsersList page + component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
+
+const USERS = [
+  {
+    id: 'u1',
+    name: 'Max',
+    image: 'https://placem.at/people?w=1260&h=750',
+    places: 3,
+  },
+];
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
