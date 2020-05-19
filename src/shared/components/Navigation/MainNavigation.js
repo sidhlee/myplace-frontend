4421aab016127fdda9b19574cd16813480e7a554
Hayoun Lee <sidhlee@gmail.com>
2020-05-19 18:13:40 +0900

    43. Adding a MainHeader

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './MainNavigation.css';
+import MainHeader from './MainHeader';
+
+const MainNavigation = (props) => {
+  return (
+    <MainHeader
+      className={`main-navigation ${props.className}`}
+      style={props.style}
+    >
+      <button className="main-navigation__menu-btn">
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">YourPlaces</Link>
+      </h1>
+      <nav>...</nav>
+    </MainHeader>
+  );
+};
+
+export default MainNavigation;
