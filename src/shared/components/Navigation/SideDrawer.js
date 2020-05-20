004f75757e4b63758c39257d58cad29f710554c3
Hayoun Lee <sidhlee@gmail.com>
2020-05-20 10:32:57 +0900

    46. Understanding Portals

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 import './SideDrawer.css';
 
 const SideDrawer = (props) => {
-  return (
+  const content = (
     <aside className={`side-drawer ${props.className}`} style={props.style}>
       {props.children}
     </aside>
   );
+  // Semantically, side-drawer should be a sibling to our App instead of its child
+  // because side-draw can be on top of the entire App components
+  // 'content' is still part of the Virtual DOM tree, but is rendered at some place else.
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
