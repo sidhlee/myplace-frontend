28e01248a548b6d70af5a71fba666369fcfba707
Hayoun Lee <sidhlee@gmail.com>
2020-05-21 11:13:38 +0900

    57. Managing state in the input component

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.css';
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.val,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
+
 const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false,
+  });
+
+  const handleChange = (e) => {
+    dispatch({ type: 'CHANGE', val: e.target.value });
+  };
+
   const element =
     props.element === 'input' ? (
-      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={handleChange}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} /> // fallback to 3
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={handleChange}
+        value={inputState.value}
+      /> // fallback to 3
     );
 
   return (
-    <div className={`form-control ${props.className}`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && 'form-control--invalid'
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
