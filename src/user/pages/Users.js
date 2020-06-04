f8fc8740103bf792e854e716542500e4ecf16267
Hayoun Lee <sidhlee@gmail.com>
2020-06-04 10:30:39 +0900

    149. Getting users with a GET request

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
-
-const USERS = [
-  {
-    id: 'u1',
-    name: 'Jacky',
-    image: 'https://placem.at/people?w=1260&h=750',
-    places: 3,
-  },
-];
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-  return <UsersList items={USERS} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        // fetch is a GET method by default
+        const response = await fetch('http://localhost:5000/api/users');
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          // throw our own error for error response
+          throw new Error(responseData.message);
+        }
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+    // state is not a dependency!
+  }, []);
+
+  const clearError = () => {
+    setError(null);
+  };
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
