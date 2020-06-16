c8842ad55f714b10bb6347b945e7d0ca15b63009
Hayoun Lee <sidhlee@gmail.com>
2020-06-16 22:26:50 +0900

    184. Adding auto-login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -34,8 +34,15 @@ const App = () => {
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem('userData');
   }, []);
 
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]); // login never changes (useCallback) => only runs on mount
   let routes;
 
   if (token) {
