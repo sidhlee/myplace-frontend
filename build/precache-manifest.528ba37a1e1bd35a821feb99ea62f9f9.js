de808dfe313224c4ca8ffb189605eb4b516bcdf7
Hayoun Lee <sidhlee@gmail.com>
2020-06-17 22:48:34 +0900

    196. Building the React app

---
+++ build/precache-manifest.528ba37a1e1bd35a821feb99ea62f9f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e95765f6882824f0a6970411c984078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ec8f1d71a468b72c458",
+    "url": "/static/css/main.3f1168a5.chunk.css"
+  },
+  {
+    "revision": "d950aa35b5e07da11441",
+    "url": "/static/js/2.e0f74666.chunk.js"
+  },
+  {
+    "revision": "3ec8f1d71a468b72c458",
+    "url": "/static/js/main.267ef9e8.chunk.js"
+  },
+  {
+    "revision": "f8b20e3499ba3d4fedbd",
+    "url": "/static/js/runtime-main.9297b71a.js"
+  }
+]);
