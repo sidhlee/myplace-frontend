add23a4b462204616e6d0cdc982abeac4a97201d
Hayoun Lee <sidhlee@gmail.com>
2020-06-17 10:57:30 +0900

    187. Creating a custom auth hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,93 +12,11 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
-
-// timeout id
-// We want to keep this data after re-rendering the App component
-let logoutTimer;
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  const [token, setToken] = useState(null);
-  // store expiration date as state
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-  const [userId, setUserId] = useState(null);
+  const { userId, token, login, logout } = useAuth();
 
-  // useCallback to prevent infinite loop from consuming component that runs login function
-  // 1. consuming component runs login in effect
-  // 2. App's isLoggedIn local state changes
-  // 3. App re-renders => new login function created
-  // 4. consuming component runs login in effect
-  const login = useCallback((uid, token, expirationDate) => {
-    // store token and userId in state
-    setToken(token); // The server sends token in the responseData when authenticated
-    setUserId(uid);
-    // this is not the App state but the local variable
-    const tokenExpirationDate =
-      // token is set to expire in 1h on backend
-      // auto-logged in with useEffect(old expiration date) || logged in through the login form (new)
-      expirationDate || new Date(new Date().getTime() + 3600 * 1000);
-    // set expiration date in the state
-    // triggers logout timer in useEffect
-    setTokenExpirationDate(tokenExpirationDate);
-
-    localStorage.setItem(
-      'userData',
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        // overwrite expiration with currently set expiration
-        // toISOString() - ensure that no date info is lost when stringified
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    // clear token & userId from state
-    setToken(null);
-    setUserId(null);
-    // clear expirationDate
-    // manual login updates tokenExpirationDate with existing state
-    // and auto-logout will run with negative remaining time
-    setTokenExpirationDate(null);
-    // clear userData from localStorage - Ensure that users stay logged out after page reload
-    localStorage.removeItem('userData');
-  }, []);
-
-  // Auto-logout effect
-  // When tokenExpirationDate is updated inside login() through auto-login | login form,
-  // logout after remaining time if token and tokenExpirationDate exist.
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      // we need to set the duration of timeout
-      const remainingTime =
-        tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      // If token or tokenExpirationDate don't exist, clear timeout.
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logout, tokenExpirationDate]);
-
-  // Auto-login effect
-  // This will run when App component is mounted (initial load & page reload)
-  useEffect(() => {
-    // storedDate.expiration is stringified.
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-
-    if (
-      storedData && // If 'userData' exists in localStorage (= logged in )
-      storedData.token && // if token is still available
-      new Date() < new Date(storedData.expiration) // check for expiration
-    ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        // login with previously set expiration
-        new Date(storedData.expiration)
-      );
-    }
-  }, [login]); // login never changes (useCallback) => only runs on mount
   let routes;
 
   if (token) {
